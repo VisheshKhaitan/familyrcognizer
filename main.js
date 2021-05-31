c1dc35b8ac56d485ae2a5d9eeb6ec60c54748463
VisheshKhaitan <78291797+VisheshKhaitan@users.noreply.github.com>
2021-05-31 18:53:59 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,18 @@
+Webcam.set({
+    width : 350,
+    height : 300,
+    image_format : 'jpeg',
+    jpeg_quality : 1080
+});
+
+   camera = document.getElementById("camera");
+   Webcam.attach('#camera');
+
+   function take_snapshot()
+   {
+       Webcam.snap(function(data_uri){
+       document.getElementById("result").innerHTML = '<img id = "captured_image" src ="'+data_uri+'">';
+       });
+   }
+   console.log('ml5 version:', ml5.version);
+   classifier = ml5.imageClassifire('https://teachablemachine.withgoogle.com/models/EC-LgErLP/model.json',modelLoaded);
